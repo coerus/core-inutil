217f2db681c7d749881e24a05a013e4663891b80
Coerus <77758986+coerus@users.noreply.github.com>
2022-07-17 20:50:00 -0300

    Add files via upload

---
+++ yes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (int argc, char *argv[])
+{
+   // ESSE COMANDO APENAS SIMULA O YES!NÃO É FUNCIONAL.
+   for (int i = 0; i < 100; i++) {
+      printf("yes\n");
+   }
+   return 0;
+}
